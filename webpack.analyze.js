285aa749870d8f1fc9726448a9d953acbd97b7f3
roshal <roshal@users.noreply.github.com>
2020-03-03 14:28:14 +0300

    add webpack-merge package

--- webpack.analyze.js
+++ webpack.analyze.js
@@ -1,39 +1,10 @@
-const path = require('path');
+const merge = require('webpack-merge');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-module.exports = {
-  entry: {
-    main: './demo/index.tsx',
-  },
-  output: {
-    path: path.resolve('dist', 'demo'),
-    filename: '[name].js',
-  },
-  resolve: {
-    extensions: [
-      '.js', '.jsx',
-      '.ts', '.tsx',
-    ],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              transpileOnly: true,
-            },
-          },
-        ],
-      },
-    ],
-  },
-  optimization: {
-    usedExports: true,
-  },
+const config = require('./webpack.config');
+
+module.exports = merge(config, {
   plugins: [
     new BundleAnalyzerPlugin(),
   ],
-};
+});

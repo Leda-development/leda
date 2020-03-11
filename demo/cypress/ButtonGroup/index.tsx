a9ba2e4b6e37f0d8b1add0741b7f3e4e228cb515
Литвинов Степан Сергеевич <litvinov.ss@esphere.ru>
2020-03-11 19:18:21 +0300

    ButtonGroup-tests

--- demo/cypress/ButtonGroup/index.tsx
+++ demo/cypress/ButtonGroup/index.tsx
@@ -5,25 +5,27 @@ import { DataObject } from '../../../leda/commonTypes';
 
 export const ButtonGroup = (): React.ReactElement => {
   const [value, setValue] = React.useState<string | undefined>();
-  const [value1, setValue1] = React.useState<string | undefined>(null);
+  const [value1, setValue1] = React.useState<string[] | undefined>(null);
   const [value2, setValue2] = React.useState<DataObject | undefined>(null);
-  const [isDisabled, setIsDisabled] = React.useState<boolean>(false);
-  
+  // const [value3, setValue3] = React.useState<number | undefined>(null);
+
   return (
     <L.Div _demoStory>
       <L.ButtonGroup
-        data={['one', 'two', 'three']}
+        data={['one', 'two', 'three', 'four']}
         defaultValue={'three'}
         onChange={(event) => setValue(event.component.value)}
         textField="data"
+        data-test="FourButtonGroup"
         _primary
       />
       <br />
       <br />
       <L.ButtonGroup
-        data={[1, 2, 3]}
+        data={[2, 3]}
         isDisabled
-        defaultValue={[2, 3]}
+        defaultValue={[2]}
+        data-test="TwoButtonGroup"
         type="checkbox"
       />
       <br />
@@ -35,32 +37,45 @@ export const ButtonGroup = (): React.ReactElement => {
           { data: 'three' },
         ]}
         defaultValue={[
-        { data: 'one' },
-      ]}
-      type="radio"
+          { data: 'one' },
+        ]}
+        type="radio"
+        data-test="ThreeButtonGroup"
         textField="data"
         _warning
       />
       <br />
       <br />
       <L.ButtonGroup
-        data={['one', 'two', 'three']}
+        data={['one']}
+        data-test="OneButtonGroup"
         value={value1}
         onChange={(event) => setValue1(event.component.value)}
       />
       <br />
       <br />
+
+      <L.ButtonGroup
+        data={[1, 2, 3]}
+        data-test="NumberButtonGroup"
+        type='checkbox'
+        
+      />
+      <br />
+      <br />
+
       <L.ButtonGroup
         data={[
           { data: 'one' },
           { data: 'two' },
           { data: 'three' },
         ]}
-        value={value2}    
+        data-test="RadioButtonGroup"
+        value={value2}
         type="radio"
         textField="data"
         onChange={(event) => setValue2(event.component.value)}
-        />
+      />
       <br />
       <br />
       {/* <L.Switcher value={isDisabled} onChange={(event) => setIsDisabled(event.component.value)}>isDisabled</L.Switcher> */}

8e8956bc766b0314404673e0fa2787605c3adcfa
Andrew D <Andrew5282011@gmail.com>
2020-03-31 12:49:36 +0300

    ProgressBar: add custom render for value render (#112)
    * ProgressBar: add custom render for value render
    
    * ProgressBar: update snapshots
    
    * ProgressBar: add theme for Value element
    
    * ProgressBar: fix types
    
    * ProgressBar: update types
    
    * ProgressBar: update snapshots
    
    Co-authored-by: Artem Povolskikh <povolskikh.aa@esphere.ru>

--- leda/components/ProgressBar/index.tsx
+++ leda/components/ProgressBar/index.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Div } from '../Div';
 import {
-  bindFunctionalRef, getClassNames, useProps, useTheme,
+  bindFunctionalRef, getClassNames, useElement, useProps, useTheme,
 } from '../../utils';
 import { COMPONENTS_NAMESPACES } from '../../constants';
 import { ProgressBarProps, ProgressBarRefCurrent } from './types';
+import { Span } from '../Span';
+import { LedaContext } from '../LedaProvider';
 
 export const ProgressBar = React.forwardRef((props: ProgressBarProps, ref?: React.Ref<ProgressBarRefCurrent>) => {
   const {
     value,
     className,
     theme: themeProp,
+    valueRender,
     ...restProps
   } = useProps(props);
 
@@ -18,7 +21,16 @@ export const ProgressBar = React.forwardRef((props: ProgressBarProps, ref?: Reac
 
   const theme = useTheme(themeProp, COMPONENTS_NAMESPACES.progressBar);
 
+  const { renders: { progressBar: customRenders } } = useContext(LedaContext);
+
   const wrapperClassNames = getClassNames(className, theme.wrapper);
+
+  const Value = useElement(
+    'Value',
+    Span,
+    valueRender ?? customRenders.valueRender,
+    props,
+  );
 
   return (
     <Div
@@ -29,7 +41,9 @@ export const ProgressBar = React.forwardRef((props: ProgressBarProps, ref?: Reac
       }))}
     >
       <div className={theme.fill} style={{ width }}>
-        {value > 10 && width}
+        <Value data-value={value} className={theme.value}>
+          {value > 10 && width}
+        </Value>
       </div>
     </Div>
   );

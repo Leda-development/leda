c94b3f2f151df9f556236be9542e4087e8421e2a
Artem Povolskikh <povolskikh.aa@esphere.ru>
2020-04-28 10:18:45 +0300

    Demo bug

--- demo/Demo.tsx
+++ demo/Demo.tsx
@@ -14,65 +14,107 @@ import {
 } from './components';
 import { useElementRef } from '../leda/utils';
 
-export const Demo = hot(() => (
-  <L.Div style={{ color: 'black' }} _wrapper>
-    {/*<Navigation />*/}
-    <L.Div _container>
-      <L.Div _row _demoMainTitle _noGutters>
-        <L.H1 _colMd12>Demo sandbox</L.H1>
-        <L.P _colMd12>
-          Специально созданная страница для тестирования компонентов, дебага и написания кода
-        </L.P>
+
+
+
+export const Demo = hot(() => {
+  const [element4, ref4] = useElementRef();
+  const [activeStep, setActiveStep] = React.useState<string | number | null>(null);
+
+
+  const t = {
+    stepKey: '1',
+    content: (props) => (
+      <L.Div _inner>
+        <L.H1>Заголовок 1</L.H1>
+        какой-то текст тут
+        <L.Ul _list-h>
+          <L.Li>
+            <L.Button onClick={props.stopTour}>
+              Закрыть
+            </L.Button>
+          </L.Li>
+          <L.Li>
+            <L.Button _success onClick={props.next}>
+              Далее
+            </L.Button>
+          </L.Li>
+        </L.Ul>
       </L.Div>
-      <HTMLTags />
-      <A />
-      <AutoComplete />
-      <Button />
-      <ButtonGroup />
-      <CheckBox />
-      <Collapse />
-      <Collapsible />
-      <Currency />
-      <DateTimePicker />
-      <DateTimeRange />
-      <DatePicker />
-      <DateRange />
-      <Div />
-      <Dl />
-      <DropDown />
-      <DropDownLink />
-      <DropDownSelect />
-      <DropZone />
-      <FileDrop />
-      <FileUpload />
-      <Input />
-      <LedaProvider />
-      <Loader />
-      <MaskedInput />
-      <Modal />
-      <MultiSelect />
-      <Notifications />
-      <NumericRange />
-      <NumericTextBox />
-      <Pagination />
-      <Password />
-      <ProgressBar />
-      <Radio />
-      <Rating />
-      <Slider />
-      <StatusBar />
-      <StickyPanel />
-      <Switcher />
-      <Table />
-      <Tabs />
-      <Tags />
-      <Textarea />
-      <TimePicker />
-      <TimeRange />
-      <Tooltip />
-      <Tour />
-      <Validation />
-      <VStepper />
+    ),
+    position: 'top',
+    element: element4,
+  };
+  return (
+    <L.Div style={{color: 'black'}} _wrapper>
+      {/*<Navigation />*/}
+      <L.Div _container>
+        <L.Div _row _demoMainTitle _noGutters>
+          <L.H1 _colMd12 ref={ref4}>Demo sandbox</L.H1>
+          <L.Button _warning onClick={() => setActiveStep('1')}>
+            Начать гайд-тур
+          </L.Button>
+
+          <L.Tour
+            data={[t]}
+            activeStepKey={activeStep}
+            onChange={(ev) => setActiveStep(ev.component.value)}
+          />
+
+          <L.P _colMd12>
+            Специально созданная страница для тестирования компонентов, дебага и написания кода
+          </L.P>
+        </L.Div>
+        <HTMLTags/>
+        <A/>
+        <AutoComplete/>
+        <Button/>
+        <ButtonGroup/>
+        <CheckBox/>
+        <Collapse/>
+        <Collapsible/>
+        <Currency/>
+        <DateTimePicker/>
+        <DateTimeRange/>
+        <DatePicker/>
+        <DateRange/>
+        <Div/>
+        <Dl/>
+        <DropDown/>
+        <DropDownLink/>
+        <DropDownSelect/>
+        <DropZone/>
+        <FileDrop/>
+        <FileUpload/>
+        <Input/>
+        <LedaProvider/>
+        <Loader/>
+        <MaskedInput/>
+        <Modal/>
+        <MultiSelect/>
+        <Notifications/>
+        <NumericRange/>
+        <NumericTextBox/>
+        <Pagination/>
+        <Password/>
+        <ProgressBar/>
+        <Radio/>
+        <Rating/>
+        <Slider/>
+        <StatusBar/>
+        <StickyPanel/>
+        <Switcher/>
+        <Table/>
+        <Tabs/>
+        <Tags/>
+        <Textarea/>
+        <TimePicker/>
+        <TimeRange/>
+        <Tooltip/>
+        <Tour/>
+        <Validation/>
+        <VStepper/>
+      </L.Div>
     </L.Div>
-  </L.Div>
-));
+  );
+});

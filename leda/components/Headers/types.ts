ea44c20a7c512cfeed8416d91d9c761a1627471e
Artem Povolskikh <Apollo-11@users.noreply.github.com>
2020-04-28 15:50:29 +0300

    Header: fix ref types (#160)
    Co-authored-by: Artem Povolskikh <povolskikh.aa@esphere.ru>

--- leda/components/Headers/types.ts
+++ leda/components/Headers/types.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 export interface HeaderProps extends React.HTMLAttributes<HTMLHeadingElement> {
-  ref?: HeaderRefCurrent,
+  ref?: React.Ref<HeaderRefCurrent>,
   shouldRender?: boolean,
   [x: string]: unknown,
 }

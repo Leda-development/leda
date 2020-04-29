d556ecb640e92c49946e23501f971b5d389b5525
Artem Povolskikh <Apollo-11@users.noreply.github.com>
2020-04-29 17:10:20 +0300

    FileDrop: update demo (#164)
    Co-authored-by: Artem Povolskikh <povolskikh.aa@esphere.ru>

--- demo/components/FileDrop/Basic.tsx
+++ demo/components/FileDrop/Basic.tsx
@@ -5,6 +5,18 @@ import { FileDropError } from '../../../leda/components/FileDrop/types';
 export const BasicFileDrop = (props: { title: string }) => {
   const [file, setFile] = React.useState<File | null>(null);
   const [error, setError] = React.useState<FileDropError>(null);
+  const [fileHref, setFileHref] = React.useState('');
+
+  React.useEffect(() => {
+    if (file == null) return;
+
+    const fileUrl = URL.createObjectURL(file);
+    setFileHref(fileUrl);
+
+    return () => {
+      URL.revokeObjectURL(fileUrl);
+    }
+  }, [file])
 
   return (
     <L.Div _box _inner>
@@ -29,6 +41,12 @@ export const BasicFileDrop = (props: { title: string }) => {
       >
         Set null
       </L.Button>
+
+      <br />
+      <br />
+
+      <L.A href={fileHref} download >{file?.name}</L.A>
+
     </L.Div>
   );
 };

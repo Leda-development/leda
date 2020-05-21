a76b188844389a579fb7f674eb3b242fe58311f8
v.volovikov <v.volovikov@gmail.com>
2020-05-21 11:18:23 +0300

    hotfix

--- leda/components/ButtonGroup/ButtonGroup.test.tsx
+++ leda/components/ButtonGroup/ButtonGroup.test.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
 import {
   render,
-  screen,
 } from '@testing-library/react';
 import { ButtonGroup } from './index';
-import { Input } from '../Input';
 
 
 describe('Check ButtonGroup snapshots collection', () => {
   test('is ButtonGroup changed?', () => {
-    const { container } = render(
-      <ButtonGroup name="buttonGroup" data={['Petya', 'Vasya', 'Oleg']} />
-    );
-    const buttonGroup = container.firstChild;
-    
     const wrapper = render((
-      <ButtonGroup name="buttonGroup" data={['Petya', 'Vasya', 'Oleg']}></ButtonGroup>
+      <ButtonGroup name="buttonGroup" data={['Petya', 'Vasya', 'Oleg']} />
     ));
-    expect(getByText('button'))
-      .toHaveLength(3);
-
     expect(wrapper.container)
       .toMatchSnapshot();
   });

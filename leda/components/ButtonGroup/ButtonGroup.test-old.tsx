12bd825145d7b32056582d88f9148b0670a13754
v.volovikov <v.volovikov@gmail.com>
2020-05-22 10:06:42 +0300

    hotfix

--- leda/components/ButtonGroup/ButtonGroup.test-old.tsx
+++ leda/components/ButtonGroup/ButtonGroup.test-old.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import { ButtonGroup } from './index';
-import { Button } from '../Button';
 
 
 describe('ButtonGroup SNAPSHOTS', () => {

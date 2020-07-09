b341ed46ae7d8c9c6d5c7b4942c74472a7ba6858
Artem Povolskikh <povolskikh-artem@yandex.ru>
2020-07-09 22:44:05 +0300

    Lint requires a test

--- leda/components/Calendar/Calendar.test.tsx
+++ leda/components/Calendar/Calendar.test.tsx
@@ -2,3 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { ChangeEvent } from './types';
+
+describe('Calendar', () => {
+  test('something', () => {
+    expect(true).toBeTruthy();
+  });
+});

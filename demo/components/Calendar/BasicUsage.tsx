33917f23780082cd671c3f89a2381fb6b71e9cf1
Artem Povolskikh <povolskikh-artem@yandex.ru>
2020-08-16 11:31:53 +0300

    Calendar: add some basic tests

--- demo/components/Calendar/BasicUsage.tsx
+++ demo/components/Calendar/BasicUsage.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 import * as L from '../../../leda';
 
 export const BasicUsage = (props: any) => {
-  const [value, setValue] = React.useState<Date>(new Date);
+  const [value, setValue] = React.useState<Date>(new Date());
+  const [hasTodayButton, setHasTodayButton] = React.useState<boolean>(false);
 
   return (
     <L.Div _box _inner _demoBg>
@@ -14,6 +15,25 @@ export const BasicUsage = (props: any) => {
         }}
         hasTodayButton
       />
+
+      <br />
+      <br />
+
+      <L.Button
+        onClick={() => {
+          setValue(new Date('09.14.2020'))
+        }}
+      >
+        Set 09.14.2020
+      </L.Button>
+      {' '}
+      <L.Button
+        onClick={() => {
+          setHasTodayButton(!hasTodayButton)
+        }}
+      >
+        Toggle hasTodayButton
+      </L.Button>
     </L.Div>
   );
 };
--- leda/components/Calendar/Calendar.test.tsx
+++ leda/components/Calendar/Calendar.test.tsx
@@ -1,10 +1,86 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { ChangeEvent } from './types';
+import { Calendar } from './index';
 
-describe('Calendar', () => {
-  test('something', () => {
-    expect(true).toBeTruthy();
+const months = [''];
+const weekdaysShortNames = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+
+describe('Calendar initial render', () => {
+  describe('in controlled mode', () => {
+    test('should have all date cells in 6-weeks view', () => {
+      const container = render(
+        <Calendar
+          onChange={() => {}}
+          value={new Date('08.14.2020')}
+        />,
+      );
+      const dateCells = container.queryAllByText(/^\d{1,2}$/);
+
+      expect(dateCells).toHaveLength(42); // 6-weeks view
+    });
+    test('should have all date cells in 5-weeks view', () => {
+      const container = render(
+        <Calendar
+          onChange={() => {}}
+          value={new Date('09.14.2020')}
+        />,
+      );
+      const dateCells = container.queryAllByText(/^\d{1,2}$/);
+
+      expect(dateCells).toHaveLength(35); // 5-weeks view
+    });
+    test('should have month name and year in the header', () => {
+      const container = render(
+        <Calendar
+          onChange={() => {}}
+          value={new Date('09.14.2020')}
+        />,
+      );
+      const header = container.getByText('Сентябрь 2020');
+
+      expect(header).toBeDefined();
+    });
+    test('should have weekdays names', () => {
+      const container = render(
+        <Calendar
+          onChange={() => {}}
+          value={new Date('09.14.2020')}
+        />,
+      );
+
+      const weekdays = weekdaysShortNames.map((weekday) => container.queryByText(weekday));
+
+      weekdays.forEach((weekday) => expect(weekday).not.toBeNull());
+    });
+  });
+});
+
+describe('Calendar props', () => {
+  describe('hasTodayButton', () => {
+    test('should show today button', () => {
+      const container = render((
+        <Calendar
+          hasTodayButton
+          value={new Date('09.14.2020')}
+          onChange={() => {}}
+        />
+      ));
+
+      const todayButton = container.queryByText(/^\d\d\s[а-я]+\s\d{4}$/);
+
+      expect(todayButton).not.toBeNull();
+    });
+    test('should not show today button without hasTodayButton prop', () => {
+      const container = render((
+        <Calendar
+          value={new Date('09.14.2020')}
+          onChange={() => {}}
+        />
+      ));
+
+      const todayButton = container.queryByText(/^\d\d\s[а-я]+\s\d{4}$/);
+
+      expect(todayButton).toBeNull();
+    });
   });
 });

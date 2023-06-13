31b1077a69d946595c2c20139d291bf2f2e8b02c
Artem Povolskikh <artem.povolskikh@gmail.com>
2023-06-13 15:46:55 +0300

    LED-68 New theme activated, no way back + small fixes (#12)
    * LED-68 New theme activaled, no way back + small fixes
    
    * LED-68 Update colors comments

--- leda/components/ButtonGroup/theme.ts
+++ leda/components/ButtonGroup/theme.ts
@@ -1,15 +1,12 @@
 /* eslint-disable key-spacing */
 export const defaultButtonGroupTheme = {
   // todo replace classes when base theme is ready
-  button:                     'button-group-item',
-  // button:                     'ld__button-group-item',
+  button:                     'ld-button-group-item',
   buttonActive:               'active',
   buttonFirst:                'first',
   buttonLast:                 'last',
-  buttonsWrapper:             'button-group-buttons-wrapper',
-  // buttonsWrapper:             'ld__button-group-buttons-wrapper',
-  wrapper:                    'button-group-wrapper',
-  // wrapper:                    'ld__button-group-wrapper',
+  buttonsWrapper:             'ld-button-group-buttons-wrapper',
+  wrapper:                    'ld-button-group-wrapper',
   wrapperDisabled:            'disabled',
   wrapperRequired:            'required',
 };

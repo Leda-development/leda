bd7ebda38ff2150a1662af0783b4e0e162588cf4
artem.povolskikh <artem.povolskikh@gmail.com>
2024-01-14 18:59:36 +0300

    LED-183 Doc version update

--- docs/src/components/header/index.tsx
+++ docs/src/components/header/index.tsx
@@ -42,7 +42,7 @@ export const MainHeader = ({
           A
         </span>
       </span>
-      <span className="ml-2 text-xs">1.2.0</span>
+      <span className="ml-2 text-xs">1.2.1</span>
     </L.A>
     <L.A
       href="https://github.com/leda-development/leda"

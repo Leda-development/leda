afec3a69133018c95fd8485e9bb7d2e6b9853df7
Artem Povolskikh <artem.povolskikh@gmail.com>
2024-06-03 11:43:16 +0300

    Fix docs build

--- docs/src/app/form-components/dropdown-select/TranslateIcon.tsx
+++ docs/src/app/form-components/dropdown-select/TranslateIcon.tsx
@@ -1,20 +1,18 @@
-// @typescript-eslint-disable-file
+/* eslint-disable */
 
 export const TranslateIcon = ({
   styles,
   ...rest
 }:{
-  styles?: Record<string, string | number>
-}) => {
-  return (
-    <svg style={styles} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" width="256" height="256" viewBox="0 0 256 256" xmlSpace="preserve" {...rest}>
+  styles?: Record<string, string | number>,
+}) => (
+  <svg style={styles} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" width="256" height="256" viewBox="0 0 256 256" xmlSpace="preserve" {...rest}>
+    {/* @ts-ignore */}
+    <g styles="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
       {/* @ts-ignore */}
-      <g styles="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
-        {/* @ts-ignore */}
-        <path d="M 47.646 58.714 c -3.399 -2.727 -6.565 -5.685 -9.477 -8.831 c 7.197 -9.938 13.357 -20.668 17.002 -30.123 H 72.97 c 1.657 0 3 -1.343 3 -3 s -1.343 -3 -3 -3 H 43.006 V 6.709 c 0 -1.657 -1.343 -3 -3 -3 s -3 1.343 -3 3 v 7.051 H 3 c -1.657 0 -3 1.343 -3 3 s 1.343 3 3 3 h 45.699 c -3.364 8.009 -8.566 16.968 -14.574 25.416 c -4.334 -5.424 -7.947 -11.301 -10.694 -17.474 c -0.674 -1.514 -2.448 -2.195 -3.96 -1.521 c -1.514 0.673 -2.195 2.447 -1.521 3.96 c 3.165 7.112 7.385 13.858 12.488 20.02 c -6.119 7.967 -12.796 15.172 -19.071 20.253 c -1.288 1.042 -1.486 2.932 -0.443 4.219 c 0.593 0.732 1.459 1.112 2.333 1.112 c 0.663 0 1.331 -0.219 1.886 -0.669 c 6.444 -5.219 13.177 -12.385 19.371 -20.327 c 2.906 3.067 6.036 5.962 9.378 8.643 c 0.554 0.444 1.217 0.66 1.875 0.66 c 0.879 0 1.749 -0.384 2.342 -1.122 C 49.146 61.639 48.938 59.751 47.646 58.714 z" styles="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
-        {/* @ts-ignore */}
-        <path d="M 71.439 29.024 c -10.234 0 -18.561 8.327 -18.561 18.561 V 83.29 c 0 1.657 1.343 3 3 3 s 3 -1.343 3 -3 V 62.279 H 84 V 83.29 c 0 1.657 1.343 3 3 3 s 3 -1.343 3 -3 V 47.585 C 90 37.351 81.674 29.024 71.439 29.024 z M 58.879 56.279 v -8.694 c 0 -6.926 5.635 -12.561 12.561 -12.561 S 84 40.659 84 47.585 v 8.694 H 58.879 z" styles="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
-      </g>
-    </svg>
-  );
-};
+      <path d="M 47.646 58.714 c -3.399 -2.727 -6.565 -5.685 -9.477 -8.831 c 7.197 -9.938 13.357 -20.668 17.002 -30.123 H 72.97 c 1.657 0 3 -1.343 3 -3 s -1.343 -3 -3 -3 H 43.006 V 6.709 c 0 -1.657 -1.343 -3 -3 -3 s -3 1.343 -3 3 v 7.051 H 3 c -1.657 0 -3 1.343 -3 3 s 1.343 3 3 3 h 45.699 c -3.364 8.009 -8.566 16.968 -14.574 25.416 c -4.334 -5.424 -7.947 -11.301 -10.694 -17.474 c -0.674 -1.514 -2.448 -2.195 -3.96 -1.521 c -1.514 0.673 -2.195 2.447 -1.521 3.96 c 3.165 7.112 7.385 13.858 12.488 20.02 c -6.119 7.967 -12.796 15.172 -19.071 20.253 c -1.288 1.042 -1.486 2.932 -0.443 4.219 c 0.593 0.732 1.459 1.112 2.333 1.112 c 0.663 0 1.331 -0.219 1.886 -0.669 c 6.444 -5.219 13.177 -12.385 19.371 -20.327 c 2.906 3.067 6.036 5.962 9.378 8.643 c 0.554 0.444 1.217 0.66 1.875 0.66 c 0.879 0 1.749 -0.384 2.342 -1.122 C 49.146 61.639 48.938 59.751 47.646 58.714 z" styles="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " strokeLinecap="round" />
+      {/* @ts-ignore */}
+      <path d="M 71.439 29.024 c -10.234 0 -18.561 8.327 -18.561 18.561 V 83.29 c 0 1.657 1.343 3 3 3 s 3 -1.343 3 -3 V 62.279 H 84 V 83.29 c 0 1.657 1.343 3 3 3 s 3 -1.343 3 -3 V 47.585 C 90 37.351 81.674 29.024 71.439 29.024 z M 58.879 56.279 v -8.694 c 0 -6.926 5.635 -12.561 12.561 -12.561 S 84 40.659 84 47.585 v 8.694 H 58.879 z" styles="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " strokeLinecap="round" />
+    </g>
+  </svg>
+);
